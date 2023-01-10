52dfa1ed4db1bd642bfbf7daa14c0ff26ca22f86
Sienna <Smste623@learner.dvusd.org>
2023-01-10 07:55:40 -0700

    my file

--- VB/WpfApp1/WpfApp1/MainWindow.xaml.vb
+++ VB/WpfApp1/WpfApp1/MainWindow.xaml.vb
@@ -1,5 +1,49 @@
 ﻿Class MainWindow
-    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
-        Textbox1.Text = "hello world"
+    Private Sub Bold_Checked(ByVal sender As Object, ByVal e As RoutedEventArgs)
+        textBox1.FontWeight = FontWeights.Bold
+    End Sub
+
+    Private Sub Bold_Unchecked(ByVal sender As Object, ByVal e As RoutedEventArgs)
+        textBox1.FontWeight = FontWeights.Normal
+    End Sub
+
+    Private Sub Italic_Checked(ByVal sender As Object, ByVal e As RoutedEventArgs)
+        textBox1.FontStyle = FontStyles.Italic
+    End Sub
+
+    Private Sub Italic_Unchecked(ByVal sender As Object, ByVal e As RoutedEventArgs)
+        textBox1.FontStyle = FontStyles.Normal
+    End Sub
+
+    Private Sub IncreaseFont_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+        If textBox1.FontSize < 40 Then
+            textBox1.FontSize += 2
+        End If
+    End Sub
+    Private Sub Save_Click()
+        ' Create OpenFileDialog
+        Dim dlg As New Microsoft.Win32.OpenFileDialog()
+
+        ' Set filter for file extension and default file extension
+        dlg.DefaultExt = ".txt"
+        dlg.Filter = "Text documents (.txt)|*.txt"
+
+        ' Display OpenFileDialog by calling ShowDialog method
+        Dim result As Nullable(Of Boolean) = dlg.ShowDialog()
+
+        ' Get the selected file name and display in a TextBox
+        If result = True Then
+            ' Open document
+            Dim filename As String = dlg.FileName
+            textBox1.Text = filename
+        End If
+    End Sub
+    Private Sub DecreaseFont_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+        If textBox1.FontSize > 10 Then
+            textBox1.FontSize -= 2
+        End If
+    End Sub
+    Private Sub Exit_Click()
+        Me.Close()
     End Sub
 End Class
